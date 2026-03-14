e79bf07654d2d49918b766206b7b8a030fcc1bf5
cdellacqua <carlo.dellacqua97@gmail.com>
2026-03-14 01:28:28 +0100

    ci: include `cargo doc`

--- ci.sh
+++ ci.sh
@@ -3,4 +3,5 @@ set -eu
 
 cargo fmt --all
 cargo clippy --fix --allow-dirty --workspace --all-targets -- -D warnings -D clippy::pedantic
+RUSTDOCFLAGS="-D warnings" cargo doc --workspace --no-deps
 cargo test --workspace --locked
